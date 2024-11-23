90d9c04048e3b6d9913ed79fc875813fd2a10050
Artem <artemproris17@gmail.com>
2024-11-24 01:50:03 +0300

    Добавление и обработка категорий

--- test/test.dart
+++ test/test.dart
@@ -1,36 +1,44 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future getBalance(String token) async {
-  final Uri url = Uri.parse('https://test-go-babich.amvera.io/user/operations/balance');
+Future<List<Map<String, dynamic>>> fetchTransactions(String token) async {
+  final Uri url = Uri.parse('https://test-go-babich.amvera.io/user/operations/period'); // Укажите URL
+  final String startDate = "2024-11-01";
+  final String endDate = DateTime.now().toIso8601String().split('T').first; // Текущая дата в формате "YYYY-MM-DD"
+  print(endDate);
+  try {
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'ApiKey $token', // Токен в заголовке
+      },
+      body: jsonEncode({
+        "start_date": startDate.toString(),
+        "end_date": endDate.toString(),
+      }), 
+    );
 
-  // Выполнение GET запроса с передачей токена в заголовке
-  final response = await http.get(
-    url,
-    headers: {
-      'Authorization': 'ApiKey $token',  // Передаем токен в заголовке
-    },
-  );
+    if (response.statusCode == 200) {
+      // Декодируем тело ответа
+      final List<dynamic> jsonResponse = json.decode(response.body);
 
-  if (response.statusCode == 200) {
-    // Если запрос успешен, парсим JSON ответ
-    return (json.decode(response.body)['amount']);
-  }
-  else if (response.statusCode == 500) {
-    return 0;
-  } 
-  else {
-    // Если произошла ошибка, выбрасываем исключение с кодом ошибки
-    throw Exception('Failed to load balance. Status code: ${response.statusCode}');
+      // Преобразуем JSON в список карт
+      return jsonResponse.map((transaction) => transaction as Map<String, dynamic>).toList();
+    } else {
+      // Обработка ошибок
+      print('Ошибка: ${response.statusCode} - ${response.body}');
+      return [];
+    }
+  } catch (e) {
+    print('Ошибка при выполнении запроса: $e');
+    return [];
   }
 }
 
+
 void main() async {
-  try {
-    String token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzI0NzAwOTgsImlhdCI6MTczMjM4MzY5OCwidXNlcl9pZCI6IjEwIn0.tV26NoYXKvD0uhcq4GGMhRVyYM-Q6QHlztddZAbUF-c';
-    var balance = await getBalance(token);
-    print(balance); // Выводим полученные данные
-  } catch (e) {
-    print('Error: $e');
-  }
+  final String token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzI0ODYyNjAsImlhdCI6MTczMjM5OTg2MCwidXNlcl9pZCI6IjEwIn0.XYuuokqQvXsmxTYZdlHNAcSC7kBim9n2pXlu5Po1kXA'; // Укажите токен
+  final List<Map<String, dynamic>> transactions = await fetchTransactions(token);
+  print(transactions);
 }
